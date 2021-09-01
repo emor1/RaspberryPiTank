489275e5892d9a15e7350ee55fe078bb57cd9a79
emori <57478311+emor1@users.noreply.github.com>
2021-09-01 18:25:21 +0900

    edit

--- tank.py
+++ tank.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template,request,Response
+from flask import Flask, render_template, request, Response
 import RPi.GPIO as GPIO
 import sys
 import signal
-import time
-import cv2
+# import time
+# import cv2
 from camera import Camera
 
-prestates="stop"
+prestates = "stop"
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 def gen(camera):
     while True:
@@ -20,50 +22,61 @@ def gen(camera):
 
         if frame is not None:
             yield (b"--frame\r\n"
-                b"Content-Type: image/jpeg\r\n\r\n" + frame.tobytes() + b"\r\n")
+                   b"Content-Type: image/jpeg\r\n\r\n" + frame.tobytes() + b"\r\n")
         else:
             print("frame is none")
 
+
 def tankForward(value=255):
-            GPIO.output(23,True)
-            GPIO.output(24,False)
-            GPIO.output(22,True)
-            GPIO.output(27,False)
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    GPIO.output(22, True)
+    GPIO.output(27, False)
+
 
 def tankBackward(value=255):
-            GPIO.output(23,False)
-            GPIO.output(24,True)
-            GPIO.output(22,False)
-            GPIO.output(27,True)
+    """[summary]
+
+    Args:
+        value (int, optional): [description]. Defaults to 255.
+    """
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    GPIO.output(22, False)
+    GPIO.output(27, True)
+
 
 def tankLeft(value=255):
-            GPIO.output(23,True)
-            GPIO.output(24,False)
-            GPIO.output(22,False)
-            GPIO.output(27,True)
+
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    GPIO.output(22, False)
+    GPIO.output(27, True)
+
 
 def tankRight(value=255):
-            GPIO.output(23,False)
-            GPIO.output(24,True)
-            GPIO.output(22,True)
-            GPIO.output(27,False)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    GPIO.output(22, True)
+    GPIO.output(27, False)
+
 
 def tankStop(value=255):
-            GPIO.output(23,False)
-            GPIO.output(24,False)
-            GPIO.output(22,False)
-            GPIO.output(27,False)
+    GPIO.output(23, False)
+    GPIO.output(24, False)
+    GPIO.output(22, False)
+    GPIO.output(27, False)
 
 
-@app.route("/on",methods=["POST"])
+@app.route("/on", methods=["POST"])
 def Tank():
     if request.method == "POST":
 
-        if "forward"==request.form['name']:
+        if "forward" == request.form['name']:
             print("-"*20)
             tankForward()
             print("Forward")
-        elif "backward"==request.form['name']:
+        elif "backward" == request.form['name']:
             print("-"*20)
             print("Backward")
             tankBackward()
@@ -71,7 +84,7 @@ def Tank():
             print("-"*20)
             tankLeft()
             print("left")
-        elif "right"==request.form['name']:
+        elif "right" == request.form['name']:
             print("-"*20)
             tankRight()
             print("right")
@@ -80,8 +93,9 @@ def Tank():
             print("-"*20)
             print("STOP")
             print("-"*20)
-        prestates=request.form['name']
+        prestates = request.form['name']
     return render_template('index.html')
+
 
 def sigint_handler(signal, frame):
     app.logger.debug("Closing")
@@ -89,17 +103,19 @@ def sigint_handler(signal, frame):
     app.logger.debug("Closed")
     sys.exit(0)
 
+
 @app.route("/video_feed")
 def video_feed():
     return Response(gen(Camera()),
-            mimetype="multipart/x-mixed-replace; boundary=frame")
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, sigint_handler)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(22,GPIO.OUT)
-    GPIO.setup(23,GPIO.OUT)
-    GPIO.setup(24,GPIO.OUT)
-    GPIO.setup(27,GPIO.OUT)
-
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(27, GPIO.OUT)
 
     app.run("0.0.0.0")
